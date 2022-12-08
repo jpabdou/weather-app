2d9da48604538d74d64399c5650cb56b883078a2
jpabdou <johnpabdou@gmail.com>
2022-12-08 00:22:24 -0800

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, {useState} from 'react';
-import Location from './components/Location';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
@@ -14,12 +13,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather for {search.display ? search.display : "Any U.S. City!"}</h1>
-        <Link style={{color: "white"}} id="home" to="/">Home</Link>
-        <Link style={{color: "white"}} id="weather" to="/weather">Weather For Current City</Link>
+        <Link style={{color: "white"}} id="home" to="/weather-app">Home</Link>
+        <Link style={{color: "white"}} id="weather" to="/weather-app/weather">Weather For Current City</Link>
       </header>
       <Routes>
-          <Route path="/" element={<Form search={search} setSearch={setSearch}/>} />
-          <Route path="/weather" element={<Weather search={search} />} />
+          <Route path="/weather-app" element={<Form search={search} setSearch={setSearch}/>} />
+
+          <Route path="/weather-app/weather" element={<Weather search={search} />} />
 
       </Routes>
 
