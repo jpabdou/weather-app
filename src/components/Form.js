d23dd33ae4a8ab98ddb967297b1b4091dd734650
jab <johnpabdou@gmail.com>
2023-01-22 15:52:58 -0800

    added dropdown selection

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,24 @@ export default function Form(props) {
                     Type the U.S. city you want to search for the weather data here: <br></br>
                     <input type="text" name="searchTerm" placeholder='Type City Here' onChange={onChange} />
                 </label>
+                <br></br>
+                <label>
+                    Or select one of the major U.S. cities here: <br></br>
+                    <select name="searchTerm" onChange={onChange}>
+                        <option value="">Select City Here</option>
+                        <option value="New York City">New York City, NY</option>
+                        <option value="Chicago">Chicago, IL</option>
+                        <option value="San Francisco">San Francisco, CA</option>
+                        <option value="Los Angeles">Los Angeles, CA</option>
+                        <option value="Philadelphia">Philadelphia, PA</option>
+                        <option value="Boston">Boston, MA</option>
+                        <option value="Houston">Houston, TX</option>
+                        <option value="San Diego">San Diego, CA</option>
+                        <option value="Seattle">Seattle, WA</option>
+                        <option value="Dallas">Dallas, TX</option>
+                        <option value="Washington, D.C.">Washington, D.C.</option>
+                    </select>
+                </label><br></br>
                 <button>Search for a U.S. city</button>
             </form>
             {search.city && <Location search={search} setSearch={setSearch} />}
